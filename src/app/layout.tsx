1c71c703e0d6239fa02c556a5d6c6d725ec13d18
naingaunglinn <naingaunglinn369@gmail.com>
2024-12-25 16:55:00 +0630

    update: design fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Jost } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const jost = Jost({ 
   variable: "--font-jost", 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import Link from "next/link";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] justify-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 		<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-			<div>
-				<h1 className="text-[120px] font-bold leading-[1.15]">Naing Aung Linn</h1>
-				<p>Developer Out Of The Box</p>
-			</div>
-			<div>
-				<h2 className="text-2xl font-bold">Information</h2>
-				<ul>
-					<li>+959440410393</li>
-					<li>naingaunglinn369@gmail.com</li>
-					<li><FaGithub className="inline mr-2" /><Link href="https://github.com/naingaunglinn" className="text-green-600">Naing Aung Linn | Github</Link></li>
-					<li><FaLinkedin className="inline mr-2" /><Link href="https://linkedin.com/in/naing-aung-linn" className="text-blue-600">Naing Aung Linn | LinkedIn</Link></li>
-				</ul>
+			<div className="relative">
+				<h1 className="text-[110px] font-bold font-jost leading-[1.15] ">Naing <span className="text-[#A91D3A]">Aung</span> Linn</h1>
+				<p className="absolute bottom-0 left-[8px]">Developer Out Of The Box</p>
 			</div>
 		</main>
     </div>
