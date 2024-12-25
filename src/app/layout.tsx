8ed4f405d0083a336d87cc2bae21380dccc2a96f
naingaunglinn <naingaunglinn369@gmail.com>
2024-12-26 00:43:28 +0630

    update: pinterest domain climed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,29 @@
-import type { Metadata } from "next";
-import { Jost } from "next/font/google";
-import "./globals.css";
+    import type { Metadata } from "next";
+    import { Jost } from "next/font/google";
+    import "./globals.css";
 
-const jost = Jost({ 
-  subsets: ["latin"] 
-});
+    const jost = Jost({ 
+    subsets: ["latin"] 
+    });
 
-export const metadata: Metadata = {
-  title: "naingaunglinn",
-  description: "Developer Out Of The Box!",
-};
+    export const metadata: Metadata = {
+    title: "naingaunglinn",
+    description: "Developer Out Of The Box!",
+    };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
+    export default function RootLayout({
+    children,
+    }: Readonly<{
+    children: React.ReactNode;
+    }>) {
+    return (
     <html lang="en">
-      <body
-        className={`${jost.className} antialiased`}
-      >
-        {children}
-      </body>
+        <head>
+            <meta name="p:domain_verify" content="2f90e9ef7e9ab50b09de3e0d34812500"/>
+        </head>
+        <body className={`${jost.className} antialiased`}>
+            {children}
+        </body>
     </html>
-  );
-}
+    );
+    }
